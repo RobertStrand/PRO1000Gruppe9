975dceb7b23dc476019ad7b73509d6a9a317a4af
JoakimAaen <143522969+JoakimAaen@users.noreply.github.com>
2024-04-26 09:10:31 +0200

    Source Code

---
+++ Source Code/script.js
@@ -0,0 +1,48 @@
+// All Javascript kode hentet fra chatGPT
+
+var navbar = document.getElementById("navbar");
+var stickyNavbar = navbar.offsetTop;
+var ctaButtons = document.querySelector('.cta-buttons');
+
+window.onscroll = function() {
+    stickNavbar();
+    makeButtonsSticky();
+    adjustLogoPosition();
+};
+
+function adjustLogoPosition() {
+    var navbarHeight = navbar.offsetHeight;
+    var logo = document.getElementById('logo');
+    logo.style.marginTop = navbarHeight + 'px';  
+}
+
+// Sticky Navbar
+function stickNavbar() {
+    if (window.scrollY >= stickyNavbar) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Sticky Buttons
+function makeButtonsSticky() {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    var mobileOffset = window.innerWidth <= 768 ? 50 : 0; 
+    var stickyPosition = stickyNavbar + navbar.offsetHeight - mobileOffset;
+
+    if (scrollPosition >= stickyPosition) {
+        ctaButtons.style.position = 'fixed';
+        ctaButtons.style.top = navbar.offsetHeight + 'px';
+        ctaButtons.style.width = '100%';
+        ctaButtons.style.padding = '10px';
+    } else {
+        ctaButtons.style.position = '';
+        ctaButtons.style.top = '';
+        ctaButtons.style.width = '';
+        ctaButtons.style.padding = '';
+    }
+}
+
+window.onresize = adjustLogoPosition;
